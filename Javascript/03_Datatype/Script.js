9fc63a81585178198cad06699b85b286b1885e91
Kandula Akhil <101031731+Akhil9804@users.noreply.github.com>
2023-03-12 15:36:48 +0000

    Adding Script.js file in 03_Datatype Folder

---
+++ Javascript/03_Datatype/Script.js
@@ -0,0 +1,49 @@
+/**
+ * TODO :: Integer Datatype
+ */
+let num1 = 10;
+console.log(`Value and type of num1 :: ${num1} ${typeof num1}`);
+num1 = -10;
+console.log(`Value and type of num1 :: ${num1} ${typeof num1}`);
+num1 = 10.54;
+console.log(`Value and type of num1 :: ${num1} ${typeof num1}`);
+
+/**
+ * TODO :: Boolean Datatype
+ */
+let bool = true;
+console.log(`Value and type of bool :: ${bool} ${typeof bool}`);
+
+/**
+ * TODO :: String Literal
+ */
+let char = "a";
+console.log(`Value and type of char :: ${char} ${typeof char}`);
+let str = "Akhil";
+console.log(`Value and type of str :: ${str} ${typeof str}`);
+
+/**
+ * TODO :: Null Literal
+ */
+let nullValue = null;
+console.log(`Value and type of nullValue :: ${nullValue} ${typeof nullValue}`);
+
+/**
+ * TODO :: Undefined Literal
+ */
+let undefinedValue;
+console.log(
+  `Value and type of undefinedValue :: ${undefinedValue} ${typeof undefinedValue}`
+);
+
+/**
+ * TODO :: BigInt Value
+ */
+let bigIntValue1 = BigInt(1234);
+console.log(
+  `Value and type of bigIntValue1 :: ${bigIntValue1} ${typeof bigIntValue1}`
+);
+let bigIntValue2 = 12345n;
+console.log(
+  `Value and type of bigIntValue2 :: ${bigIntValue2} ${typeof bigIntValue2}`
+);
