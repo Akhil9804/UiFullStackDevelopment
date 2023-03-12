d9cf809e74aa58a7522c22415ed8369496bb8dc5
Kandula Akhil <101031731+Akhil9804@users.noreply.github.com>
2023-03-12 06:44:42 +0000

    Adding 01_ArithematicOperator.js file into Javascript Folder

---
+++ Javascript/02_Token/03_Operator/01_ArithematicOperator.js
@@ -0,0 +1,14 @@
+// Addition :: Integer + Integer
+let num1 = 10;
+let num2 = 20;
+console.log(`Addition of two number :: ${num1 + num2}`);
+
+// Subtraction :: Integer - Integer
+num1 = 20;
+num2 = 10;
+console.log(`Subtraction of two number :: ${num1 - num2}`);
+
+// Multiplication :: Integer * Integer
+num1 = 10;
+num2 = 2;
+console.log(`Multiplication of two number :: ${num1 * num2}`);
