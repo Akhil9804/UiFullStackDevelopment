3c61c07c47b482daa0a4080ebdd0c657b916bc9c
Kandula Akhil <101031731+Akhil9804@users.noreply.github.com>
2023-03-12 13:29:45 +0000

    Updating 01_ArithematicOperator.js file in Javascript Folder

--- Javascript/02_Token/03_Operator/01_ArithematicOperator.js
+++ Javascript/02_Token/03_Operator/01_ArithematicOperator.js
@@ -1,14 +1,47 @@
 // Addition :: Integer + Integer
 let num1 = 10;
 let num2 = 20;
+console.log(
+  `Value and type num1 and num2 :: ${num1} ${typeof num1} ${num2} ${typeof num2}`
+);
 console.log(`Addition of two number :: ${num1 + num2}`);
 
 // Subtraction :: Integer - Integer
 num1 = 20;
 num2 = 10;
+console.log(
+  `Value and type num1 and num2 :: ${num1} ${typeof num1} ${num2} ${typeof num2}`
+);
 console.log(`Subtraction of two number :: ${num1 - num2}`);
 
 // Multiplication :: Integer * Integer
 num1 = 10;
 num2 = 2;
+console.log(
+  `Value and type num1 and num2 :: ${num1} ${typeof num1} ${num2} ${typeof num2}`
+);
 console.log(`Multiplication of two number :: ${num1 * num2}`);
+
+// Division :: Integer / Integer
+num1 = 20;
+num2 = 2;
+console.log(
+  `Value and type num1 and num2 :: ${num1} ${typeof num1} ${num2} ${typeof num2}`
+);
+console.log(`Division of two number :: ${num1 / num2}`);
+
+// Modules :: Integer % Integer
+num1 = 20;
+num2 = 3;
+console.log(
+  `Value and type num1 and num2 :: ${num1} ${typeof num1} ${num2} ${typeof num2}`
+);
+console.log(`Modules of two number :: ${num1 % num2}`);
+
+// Power :: Integer ** Integer
+num1 = 2;
+num2 = 3;
+console.log(
+  `Value and type num1 and num2 :: ${num1} ${typeof num1} ${num2} ${typeof num2}`
+);
+console.log(`Power of two number :: ${num1 ** num2}`);
