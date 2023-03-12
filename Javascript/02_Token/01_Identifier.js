86738cc184876afcaa60c7185987a10aab6786b9
Kandula Akhil <101031731+Akhil9804@users.noreply.github.com>
2023-03-12 05:58:12 +0000

    Adding 01_Identifier.js file into Javascript Folder

---
+++ Javascript/02_Token/01_Identifier.js
@@ -0,0 +1,53 @@
+// firstName, lastName and age are variable Identifier
+var firstName = "Akhil";
+let lastName = "Kandula";
+const age =
+  new Date().getDate() === new Date("1998-03-14").getDate()
+    ? new Date().getFullYear() - new Date("1998-03-14").getFullYear()
+    : new Date().getFullYear() - new Date("1998-03-14").getFullYear() - 1;
+console.log("First Name :: " + firstName);
+console.warn("Last Name :: " + lastName);
+console.error(`Age :: ${age}`);
+
+// educationDetails is function declaration
+function educationDetails(
+  schoolName,
+  schoolAddress,
+  schoolYear,
+  collegeName,
+  collegeAddress,
+  collegeYear
+) {
+  // School Details
+  console.log("School Name :: " + schoolName);
+  console.warn("School Address :: " + schoolAddress);
+  console.error(`School Completion Year :: ${schoolYear}`);
+
+  //College Details
+  console.log("College Name :: " + collegeName);
+  console.warn("College Address :: " + collegeAddress);
+  console.error(`College Completion Year :: ${collegeYear}`);
+}
+
+educationDetails(
+  "Sri Chaitanya Techno School",
+  "Boyapalem,Visakhapatnam",
+  2013,
+  "Sri Chaitanya JR College",
+  "Maddilapalem, Visakhapatnam",
+  2016
+);
+
+// employeeDetails in Object Identifier
+let employeeDetails = {
+  companyName: "TCS",
+  companyLocation: "Waverock,Hyderabad",
+  salary: "4,12,000",
+  displayCompanyDetails: function () {
+    console.log("Company Name :: " + this.companyName);
+    console.warn("Company Location :: " + this.companyLocation);
+    console.error("Salary :: " + this.salary);
+  },
+};
+
+employeeDetails.displayCompanyDetails();
