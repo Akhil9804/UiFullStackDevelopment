631789bd828450ce29b324655772534582edf826
Kandula Akhil <101031731+Akhil9804@users.noreply.github.com>
2023-03-12 15:45:11 +0000

    Adding files in 04_Input Folder

---
+++ Javascript/04_Input/Script.js
@@ -0,0 +1,4 @@
+let num1 = parseInt(prompt("Enter the first value"));
+let num2 = parseInt(prompt("Enter the second value"));
+const result = num1 + num2;
+console.log("Sum of two number :: " + result);
