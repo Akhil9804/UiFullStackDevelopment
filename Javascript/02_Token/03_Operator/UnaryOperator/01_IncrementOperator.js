bb96274c6bcb7ad15bc50dc6ea9460e9eb6e51ca
Kandula Akhil <101031731+Akhil9804@users.noreply.github.com>
2023-03-12 14:27:57 +0000

    Adding file into Javascript Folder

---
+++ Javascript/02_Token/03_Operator/UnaryOperator/01_IncrementOperator.js
@@ -0,0 +1,13 @@
+/**
+ * TODO :: Increment Operator
+ */
+let num1 = 10;
+/**
+ * TODO :: Post Increment Operator
+ */
+console.log(`${num1}++ = ${num1++}`);
+console.log(`num1 :: ${num1}`);
+/**
+ * TODO :: Pre Increment Operator
+ */
+console.log(`++${num1} = ${++num1}`);
---
+++ Javascript/02_Token/03_Operator/UnaryOperator/02_DecrementOperator.js
@@ -0,0 +1,13 @@
+/**
+ * TODO :: Decrement Operator
+ */
+let num1 = 10;
+/**
+ * TODO :: Post Decrement Operator
+ */
+console.log(`${num1}-- = ${num1--}`);
+console.log(`num1 :: ${num1}`);
+/**
+ * TODO :: Pre Decrement Operator
+ */
+console.log(`--${num1} = ${--num1}`);
