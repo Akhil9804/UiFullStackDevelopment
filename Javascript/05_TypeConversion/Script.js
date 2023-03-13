4b6932a590025849c0f461092dfbf42552588843
Kandula Akhil <101031731+Akhil9804@users.noreply.github.com>
2023-03-13 04:56:33 +0000

    Updating Script.js file in 05_TypeConversion Folder

--- Javascript/05_TypeConversion/Script.js
+++ Javascript/05_TypeConversion/Script.js
@@ -26,6 +26,10 @@ num = 10.67;
 console.log(`Value and type of num :: ${num} ${typeof num}`);
 str = String(num);
 console.log(`Value and type of str :: ${str} ${typeof str}`);
+num = NaN;
+console.log(`Value and type of num :: ${num} ${typeof num}`);
+str = String(num);
+console.log(`Value and type of str :: ${str} ${typeof str}`);
 console.log();
 
 console.log("Convert Boolean to Number");
@@ -86,3 +90,30 @@ str = "";
 console.log(`Value and type of str :: ${str} ${typeof str}`);
 bool = Boolean(str);
 console.log(`Value and type of bool :: ${bool} ${typeof bool}`);
+console.log();
+
+console.log(`Convert Null, Undefined to String`);
+let nullValue = null;
+console.log(`Value and type of nullValue :: ${nullValue} ${typeof nullValue}`);
+str = String(nullValue);
+console.log(`Value and type of str :: ${str} ${typeof str}`);
+let unDefinedValue;
+console.log(
+  `Value and type of unDefinedValue :: ${unDefinedValue} ${typeof unDefinedValue}`
+);
+str = String(unDefinedValue);
+console.log(`Value and type of str :: ${str} ${typeof str}`);
+console.log();
+
+console.log(`Convert Null, Undefined to Boolean`);
+nullValue = null;
+console.log(`Value and type of nullValue :: ${nullValue} ${typeof nullValue}`);
+bool = Boolean(nullValue);
+console.log(`Value and type of bool :: ${bool} ${typeof bool}`);
+unDefinedValue;
+console.log(
+  `Value and type of unDefinedValue :: ${unDefinedValue} ${typeof unDefinedValue}`
+);
+bool = Boolean(unDefinedValue);
+console.log(`Value and type of bool :: ${bool} ${typeof bool}`);
+console.log();
