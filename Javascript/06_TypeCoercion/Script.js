ec0c1c10a4d7daaa668a79af5ce0f04da8019de0
Kandula Akhil <101031731+Akhil9804@users.noreply.github.com>
2023-03-13 17:01:09 +0000

    Updating Script.js file in 06_TypeCoercion Folder

--- Javascript/06_TypeCoercion/Script.js
+++ Javascript/06_TypeCoercion/Script.js
@@ -1,9 +1,37 @@
 /**
- * TODO :: Number Conversion to String
+ * TODO :: String Conversion
  */
-let num = 10;
-let str = "20";
-console.log(`Value and type of num :: ${num} ${typeof num}`);
+let str = "hello";
 console.log(`Value and type of str :: ${str} ${typeof str}`);
-let result = num + str;
+let bool = true;
+console.log(`Value and type of bool :: ${bool} ${typeof bool}`);
+let result = str + bool;
+console.log(`Value and type of result :: ${result} ${typeof result}`);
+
+let nullValue = null;
+console.log(`Value and type of nullValue :: ${nullValue} ${typeof nullValue}`);
+result = str + nullValue;
+console.log(`Value and type of result :: ${result} ${typeof result}`);
+
+let undefinedValue;
+console.log(
+  `Value and type of undefinedValue :: ${undefinedValue} ${typeof undefinedValue}`
+);
+result = str + undefinedValue;
+console.log(`Value and type of result :: ${result} ${typeof result}`);
+
+let num = 10;
+console.log(`Value and type of num :: ${num} ${typeof num}`);
+result = str + num;
+console.log(`Value and type of result :: ${result} ${typeof result}`);
+console.log();
+
+/**
+ * TODO :: Numeric Conversion
+ */
+num = 10;
+console.log(`Value and type of num :: ${num} ${typeof num}`);
+str = "20";
+console.log(`Value and type of str :: ${str} ${typeof str}`);
+result = str - num;
 console.log(`Value and type of result :: ${result} ${typeof result}`);
