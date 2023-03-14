3d9296d76ce96591e8a3ed802e6499909f3918c1
Kandula Akhil <101031731+Akhil9804@users.noreply.github.com>
2023-03-14 02:44:18 +0000

    Adding file in 07_Control_Statement Folder

---
+++ Javascript/07_Control_Statement/Decision_Statement/If_Else_Statement/Script.js
@@ -0,0 +1,6 @@
+var age = parseInt(prompt("Enter the Age"));
+if (age >= 18) {
+  console.log("Eligile for vote");
+} else {
+  console.error(`Not Eligible for vote`);
+}
---
+++ Javascript/07_Control_Statement/Decision_Statement/If_Statement/Script.js
@@ -0,0 +1,5 @@
+let num = prompt(parseInt("Enter num value"));
+if (num > 10) {
+  console.log(`${num} is greater than 10`);
+}
+console.log(`Value of num :: ${num}`);
